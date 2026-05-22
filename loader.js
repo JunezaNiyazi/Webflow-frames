a19e67fa80e74f7c8a72d5b29d781bd8a4337ddd
Juneza Niyazi <38693324+JunezaNiyazi@users.noreply.github.com>
2026-05-22 21:44:58 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,12 +2,12 @@
   function getProfile() {
     const w = window.innerWidth;
     const h = window.innerHeight;
-    const isMobileOrTablet = w < 1024 || (w < 768 && h > w);
+    const isMobileOrTablet = w < 1024 && h > w;
     return isMobileOrTablet ? 'mobile' : 'desktop';
   }
 
   const profile = getProfile();
-  const BASE = 'https://cdn.jsdelivr.net/gh/JunezaNiyazi/Webflow-frames@main';
+  const BASE = 'https://raw.githubusercontent.com/JunezaNiyazi/Webflow-frames/main';
 
   const script = document.createElement('script');
   script.src = `${BASE}/${profile}-frames.js`;
